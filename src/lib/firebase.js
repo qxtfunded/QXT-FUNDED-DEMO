db22c2d07c1e57da7144ce9b02522a720bbd29a7
qxtfunded <qxtfunded1@gmail.com>
2026-08-08 01:19:06 +0500

    build: upgrade Vite to v6 and update Firebase config
    Upgrade build dependencies to Vite 6 and switch to initializeFirestore with auto-polling enabled to improve connection reliability.

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
@@ -16,7 +16,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const db = initializeFirestore(app, {
+  experimentalAutoDetectLongPolling: true,
+})
 export const storage = getStorage(app)
 export const googleProvider = new GoogleAuthProvider()
 
